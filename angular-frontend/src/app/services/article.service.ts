79dbdd0c68eb8174b8cc5032d1e91bcad0a792eb
Srdjan Milic <srdjanrmilic@gmail.com>
2020-03-23 19:27:25 +0100

    fix delete row from table

--- angular-frontend/src/app/services/article.service.ts
+++ angular-frontend/src/app/services/article.service.ts
@@ -1,66 +1,74 @@
-'use strict';
+"use strict";
 
-import { Injectable, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Article } from './article';
-import { MatSnackBar, MatTableDataSource } from '@angular/material';
-import { MatPaginator } from '@angular/material/paginator';
+import { Injectable, ViewChild } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Article } from "./article";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { MatTableDataSource } from "@angular/material/table";
+import { MatPaginator } from "@angular/material/paginator";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-
 export class ArticleService {
-
-  AUTH_SERVER = 'http://localhost:3000';
+  AUTH_SERVER = "http://localhost:3000";
 
   dataSource: any = [];
 
-  constructor(public httpClient: HttpClient, public snackBar: MatSnackBar) { }
+  constructor(public httpClient: HttpClient, public snackBar: MatSnackBar) {}
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   createArticle(article: Article) {
-    return this.httpClient.post(`${this.AUTH_SERVER}/api/v1/create-article`, article).subscribe(() => {
-      this.snackBar.open('Article created!', '', {
-        duration: 2000
+    return this.httpClient
+      .post(`${this.AUTH_SERVER}/api/v1/create-article`, article)
+      .subscribe(() => {
+        this.snackBar.open("Article created!", "", {
+          duration: 2000,
+        });
+        console.log(`Submitted to database!`);
       });
-      console.log(`Submitted to database!`);
-    });
   }
 
   updateArticle(article: Article) {
-    return this.httpClient.put(`${this.AUTH_SERVER}/api/v1/update-article`, article).subscribe(() => {
-      this.snackBar.open('Article updated!', '', {
-        duration: 2000
+    return this.httpClient
+      .put(`${this.AUTH_SERVER}/api/v1/update-article`, article)
+      .subscribe(() => {
+        this.snackBar.open("Article updated!", "", {
+          duration: 2000,
+        });
       });
-    });
   }
 
   listArticles() {
-    return this.httpClient.get(`${this.AUTH_SERVER}/api/v1/list-articles`).subscribe((res: any[]) => {
-      this.dataSource = new MatTableDataSource();
-      this.dataSource = res;
-      this.dataSource.paginator = this.paginator;
-      console.log(this.dataSource);
-    });
+    return this.httpClient
+      .get(`${this.AUTH_SERVER}/api/v1/list-articles`)
+      .subscribe((res: any[]) => {
+        this.dataSource = new MatTableDataSource();
+        this.dataSource = res;
+        this.dataSource.paginator = this.paginator;
+        console.log(this.dataSource);
+      });
   }
 
   readArticle(id: number) {
-    return this.httpClient.get(`${this.AUTH_SERVER}/api/v1/list-articles/${id}`).subscribe((res: any[]) => {
-      this.dataSource = res;
-      console.log(res);
-    });
+    return this.httpClient
+      .get(`${this.AUTH_SERVER}/api/v1/list-articles/${id}`)
+      .subscribe((res: any[]) => {
+        this.dataSource = res;
+        console.log(res);
+      });
   }
 
   deleteArticle(id: number) {
-    return this.httpClient.delete(`${this.AUTH_SERVER}/api/v1/list-articles/${id}`).subscribe((res: any[]) => {
-      this.listArticles();
-      this.snackBar.open('Article deleted!', '', {
-        duration: 2000
+    return this.httpClient
+      .delete(`${this.AUTH_SERVER}/api/v1/delete-article/${id}`)
+      .subscribe((res: any[]) => {
+        this.dataSource.articles = this.dataSource.articles.filter((x) => x.id !== id);
+        this.snackBar.open("Article deleted!", "", {
+          duration: 2000,
+        });
+        console.log(res);
       });
-      console.log(res);
-    });
   }
-
 }
