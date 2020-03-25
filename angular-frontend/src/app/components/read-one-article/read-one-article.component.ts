490bc33973ac86827d8df599f0360855329ba0d8
Srdjan Milic <srdjanrmilic@gmail.com>
2020-03-25 11:11:48 +0100

    show one article after refresh

--- angular-frontend/src/app/components/read-one-article/read-one-article.component.ts
+++ angular-frontend/src/app/components/read-one-article/read-one-article.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { ArticleService } from '../../services/article.service';
+import { Component, OnInit } from "@angular/core";
+import { ArticleService } from "../../services/article.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-read-one-article',
-  templateUrl: './read-one-article.component.html',
-  styleUrls: ['./read-one-article.component.less']
+  selector: "app-read-one-article",
+  templateUrl: "./read-one-article.component.html",
+  styleUrls: ["./read-one-article.component.less"],
 })
 export class ReadOneArticleComponent implements OnInit {
+  id: number;
 
-  constructor(public articleService: ArticleService) {  }
+  constructor(
+    public articleService: ArticleService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.id = this.route.snapshot.params["id"];
+    this.articleService.readArticle(this.id);
   }
-
 }
